f5b5af8e7e3bc20d06fe9e4b05854f4bf6a5baee
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-06 22:48:14 +0400

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int numbers;
+	cout << "GReq tiv " << endl;
+	cin >> numbers;
+	for (int i = 0; i <= numbers; ++i) {
+		for (int j = 0; j < i; ++j) {
+			cout << '*';
+		}
+		cout << endl;
+	}
+	return 0;
+}
+	

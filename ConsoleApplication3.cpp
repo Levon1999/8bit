94a2ee1fc4ce2e2335916c267e66d7436f99f678
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-06 23:51:57 +0400

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+int main() {
+	std::cout << "Greq tiv " << std::endl;
+	int x = 0;
+	std::cin >> x;
+	std::cout << x << " 16-akan hamakargum = " << std::hex<<x << std::endl;
+	return 0;
+}

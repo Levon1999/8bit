0e6dbd83cbd8e418ebd73f21c686075b2d515796
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-06 23:09:52 +0400

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+#include <bitset>
+using namespace std;
+int main() {
+	cout << "Greq voreve drakan tiv " << endl;
+	unsigned int x = 0;
+	cin >> x;
+	bitset<8>inputbits(x);
+	cout << x << " Erkuakan hamakargum = " << inputbits << endl;
+	return 0;
+}

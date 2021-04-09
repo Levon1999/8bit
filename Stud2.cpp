3f0dbd21a82b76552b5f666d81f44699538f0a88
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-09 17:31:53 +0400

    Add files via upload

---
+++ Stud2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+struct Student {
+	string name;
+	string surname;
+	short age;
+	string ararka;
+};
+int main() {
+	Student Agroekologia1;
+	Agroekologia1.name = "Levon";
+	Agroekologia1.surname = "Karapetyan";
+	Agroekologia1.age = 21;
+	Agroekologia1.ararka = "Xaxoxagorcutyun";
+
+	Student Agroekologia2;
+	Agroekologia2.name = "Pargev";
+	Agroekologia2.surname = "Minasyan ";
+	Agroekologia2.age = 20;
+	Agroekologia2.ararka = "Organakan Gyux.";
+
+	if (Agroekologia1.age > Agroekologia2.age) {
+		std::cout << Agroekologia1.name;
+	}
+	else {
+		std::cout << Agroekologia2.name;
+	}
+	return 0;
+}

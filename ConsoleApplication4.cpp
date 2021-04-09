596cc938eebdb59b97f45cb7dbddd1755fed980d
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-09 17:55:06 +0400

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+struct Student {
+	std::string name;
+	std::string surname;
+	short age;
+};
+int main() {
+	Student Aek1;
+	Aek1.name = "Levon ";
+	Aek1.surname = "Karapetyan ";
+	Aek1.age = 21;
+	Student Aek2;
+	Aek2.name = "Zaven ";
+	Aek2.surname = "Minasyan ";
+	Aek2.age = 21;
+	Student Aek3;
+	Aek3.name = Aek1.name;
+	Aek3.surname = Aek2.surname;
+	Aek3.age = (Aek1.age + Aek2.age) / 2;
+	std::cout << Aek3.name
+	          << Aek3.surname
+		      <<Aek3.age;
+	return 0;
+}

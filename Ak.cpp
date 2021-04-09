e5fd2c93779e964445e69382ecd3cc0ae5a7140a
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-09 17:01:31 +0400

    Add files via upload

---
+++ Ak.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int arr[4] = { 3,5,99,4 };
+	int min = arr[0];
+	for (int i = 0; i < 4; ++i) {
+		if (min >arr[i]) {
+
+			min = arr[i];
+		}
+	}
+	cout << min;
+	return 0;
+
+}

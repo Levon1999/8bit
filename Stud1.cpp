563aba6f665cfd165c2531fa52f8481a267cdb59
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-09 17:16:55 +0400

    Add files via upload

---
+++ Stud1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+struct Student {
+	std::string name;
+	std::string surname;
+	short age;
+	std::string ararka;
+};
+int main() {
+	Student Agroekologia;
+	Agroekologia.name = "Levon ";
+	Agroekologia.surname = "karapetyan ";
+	Agroekologia.age = 21;
+	Agroekologia.ararka = "Xaxoxagorcutyun ";
+	return 0;
+}

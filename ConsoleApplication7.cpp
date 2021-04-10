47f1071d225a0612ac34d846ae70c3142e5d2dfe
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-10 16:32:17 +0400

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a = 0;
+	int x = 0, y = 0, z = 0, i = 0;
+	cin >> a;
+	x = a / 1000;
+	y = (a % 1000) / 100;
+	z = (a % 100) / 10;
+	i = a % 10;
+	cout << x << " " << y << " " << z << " "<<i ;
+	return 0;
+}

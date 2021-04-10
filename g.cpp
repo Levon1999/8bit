b8b9079218567f78a392c453a0a80d2640a01fd4
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-10 16:49:14 +0400

    Add files via upload

---
+++ g.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int x = 0;
+	int a, s, d, f;
+	cin >> x;
+	a = x / 100;
+	s = (x % 100) / 10;
+	d = x % 10;
+	f = a + s + d;
+	cout << f;
+	return 0;
+}

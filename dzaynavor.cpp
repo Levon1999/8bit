c03ecb606ea245aeba5f8480fd76c9d082c48c13
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-10 18:16:11 +0400

    Add files via upload

---
+++ dzaynavor.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s = "Vahagn";
+	char x = 'a';
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == x) {
+			cout << ' ';
+		}
+		else {
+			cout << s[i];
+		}
+	}
+	return 0;
+}

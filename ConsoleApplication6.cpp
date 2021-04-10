1a026b46c1b7bbcfee2f6833a78e34222e9c70d0
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-10 15:33:39 +0400

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main() {
+	int a=0,b=0 , x=0;
+	std::cout << "Nermucel tiv " << std::endl;
+	std::cin >> a;//123
+	while (a != 0) {
+		x = a % 10;//12.3
+		a = a / 10;//12.3/10=1.2
+		b = b * 10 + x;//10+3.2.1
+	}
+	std::cout << "b=" << b;
+	return 0;
+}

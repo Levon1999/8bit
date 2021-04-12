2e85ecd92fac83203c3c8b1ee4e600267afd0e57
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-12 16:09:20 +0400

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int arr[4] = { 2,22,6,66 };
+	int max = arr[0];
+	int min = arr[0];
+	for (int i = 0; i < 4; ++i) {
+		if (arr[i] < max) {
+			max = arr[i];
+		}
+	}
+	for (int z = 0; z < 4; ++z) {
+		if (arr[z] > min) {
+			min = arr[z];
+		}
+	}
+	cout << max +min<< endl;
+
+	return 0;
+}

fc7f5298b9093c7f9f9fc3c0c8fb5d6642d8ebf7
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-12 18:41:14 +0400

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+﻿/*grel funkcia,vor@ stanum e qarakusayin havasarman tvyalner@*/
+#include <iostream>
+using namespace std;
+void qarakusi(int a, int b) {
+	a = b * b;
+	cout << a;
+}
+int main() {
+	int z = 0;
+	int x = 0;
+	cin >> z;
+	cin >> x;
+	qarakusi(z,x);
+	return 0;
+}

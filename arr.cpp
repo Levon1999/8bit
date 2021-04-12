83750d128cbe01aea232d8026019701399a42ef8
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-12 16:29:21 +0400

    Add files via upload

---
+++ arr.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int arrr[3] = { 21,4,7 };
+	int ar[3] = { 11,44,9 };
+	int z;
+	for (int i = 0; i <= 2; ++i) {
+		z = arrr[i] * ar[i];
+		cout << z;
+}
+	return 0;
+}

c6a74c92f5ec3bb690ba42a8a71a0662d4173a55
Levon <73011326+Levon1999@users.noreply.github.com>
2021-04-14 10:25:44 +0400

    Add files via upload

---
+++ rec.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int rec_fib(int n) {
+	if (n == 1 || n == 0) {
+		return 1;
+	}
+		return rec_fib(n - 1) + rec_fib(n - 2);
+}
+int main() {
+	int n;
+	std::cin >> n;
+	std::cout << rec_fib(n);
+	return 0;
+}
